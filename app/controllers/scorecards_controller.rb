0b0e85ffeb4ec615f74faacb53a9b09d13d46691
Chris <cjw7488@gmail.com>
2011-03-09 16:15:41 -0600

    Added second cut of style to scorecard / now shows submitted rounds

--- app/controllers/scorecards_controller.rb
+++ app/controllers/scorecards_controller.rb
@@ -1,12 +1,19 @@
 class ScorecardsController < ApplicationController
   def index
     @scorecard = Scorecard.new
+    @scorecards = Scorecard.all
   end
 
   def new
   end
 
   def create
+    @scorecard = Scorecard.new(params[:scorecard])
+    
+    if @scorecard.save
+      redirect_to(:action => "index")
+    else
+    end
   end
 
 end
