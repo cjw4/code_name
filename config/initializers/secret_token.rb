8e724528b0e6e7de0ea5c18f22ad419dfc770f90
Chris <cjw7488@gmail.com>
2011-03-09 14:18:23 -0600

    Initial Commit / Rails Framework

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DiscGolf::Application.config.secret_token = 'f547831c9ac460919cbae54589eb80f38a6f53f9ec94c627ac739a81cbd291f809ea1bc6eaad8d031a52bdc44cf5b5c3bdc82ff36f150f3a1d1095927e6cce05'
