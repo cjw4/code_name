6539841ed3e792384be39895d25c297be1705235
Chris <cjw7488@gmail.com>
2011-03-10 16:20:02 -0600

    Added arrows to show/hide rounds / added color and fixed javascript NaN bug

--- public/javascripts/scorecard.js
+++ public/javascripts/scorecard.js
@@ -5,18 +5,24 @@ $(document).ready(function() {
 		for (i = 1; i <= 18; i++) {
 			
 			var holeInput = $("#scorecard_hole"+i).val();
+			holeInput = parseInt(holeInput);
 			
-			if (!isNaN(holeInput)) {
-				holeInput = parseInt(holeInput);
+			if (!isNaN(holeInput)) {		
 				total = total + holeInput;
 				score = total - 54;
 				
-				if (score > 0) score = "+" + score;	
+				if (score > 0) score = "+" + score;
 				else if (score == 0) score = "E";
 				else score = score;
 			}
 		}
 		$("#scorecard_total").val(total);
 		$("#scorecard_score").val(score);
+		
+		scoreContainer = $("#scorecard_score")
+		
+		if (score > 0) scoreContainer.css("color", "blue");
+		else if (score == "E") scoreContainer.css("color", "black");
+		else scoreContainer.css("color", "green");
 	});
 });
