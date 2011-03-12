ed885be2850e5f5ba321404d221cad2f7d511309
Chris <cjw7488@gmail.com>
2011-03-12 10:22:53 -0600

    Added some color styling to scorecard

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params[:user])
     if @user.save
-      flash[:notice] = "Successfully created account!"
+      flash[:notice] = "You've created a playElver account successfully"
       redirect_to({:controller => "scorecards", :action => "index" })
     else
       render :action => "new"
--- public/javascripts/scorecard.js
+++ public/javascripts/scorecard.js
@@ -1,28 +1,50 @@
 $(document).ready(function() {
-	$(".holeInput").bind("blur", function() {
-		var i = 1;
-		var total = 0;
-		for (i = 1; i <= 18; i++) {
-			
-			var holeInput = $("#scorecard_hole"+i).val();
-			holeInput = parseInt(holeInput);
-			
-			if (!isNaN(holeInput)) {		
-				total = total + holeInput;
-				score = total - 54;
-				
-				if (score > 0) score = "+" + score;
-				else if (score == 0) score = "E";
-				else score = score;
-			}
-		}
-		$("#scorecard_total").val(total);
-		$("#scorecard_score").val(score);
-		
-		scoreContainer = $("#scorecard_score")
-		
-		if (score > 0) scoreContainer.css("color", "blue");
-		else if (score == "E") scoreContainer.css("color", "black");
-		else scoreContainer.css("color", "green");
-	});
+	$(".holeInput").bind("blur", scorecard);
+	
+	changeOutputColors();
 });
+
+function scorecard() {
+	var i = 1;
+	var total = 0;
+	for (i = 1; i <= 18; i++) {
+
+		var holeInput = $("#scorecard_hole"+i);
+		var holeInputValue = holeInput.val();
+		holeInputValue = parseInt(holeInputValue);
+		
+		if (holeInputValue > 3) holeInput.css("color", "blue");
+		else if (holeInputValue == 3) holeInput.css("color", "black");
+		else holeInput.css("color", "red");
+		
+		if (!isNaN(holeInputValue)) {		
+			total = total + holeInputValue;
+			score = total - 54;
+			
+			if (score > 0) score = "+" + score;
+			else if (score == 0) score = "E";
+			else score = score;
+		}
+	}
+	$("#scorecard_total").val(total);
+	$("#scorecard_score").val(score);
+	
+	scoreContainer = $("#scorecard_score")
+	
+	if (score > 0) scoreContainer.css("color", "blue");
+	else if (score == "E") scoreContainer.css("color", "black");
+	else scoreContainer.css("color", "red");
+};
+
+function changeOutputColors() {
+	var holeOutput = $(".holeOutput");
+	
+	for(i=0; i < 18; i++) {
+		holeOutputElement = holeOutput[i];
+		holeOutputValue = parseInt(holeOutputElement.innerHTML);
+		if (holeOutputValue > 3) holeOutputElement.setAttribute("style", "color:blue;");
+		else if (holeOutputValue < 3) holeOutputElement.setAttribute("style", "color:red;");
+	}
+	
+	
+};
